3f4f1658fa92252318b30094e77207099ae32523
baAlex <baAlex@users.noreply.github.com>
2019-07-14 09:16:04 -0300

    Simple fog

--- source/shaders/terrain-fragment.glsl
+++ source/shaders/terrain-fragment.glsl
@@ -1,9 +1,18 @@
 #version 100
 
 uniform sampler2D color_texture;
+
 varying lowp vec2 uv;
+varying lowp vec3 vertex;
+varying lowp vec3 camera;
+
+const lowp vec4 fog_color = vec4(0.80, 0.82, 0.84, 1.0);
 
 void main()
 {
-	gl_FragColor = texture2D(color_texture, uv);
+	lowp float fog_factor = distance(vertex, camera) / 20000.0; // 20 km
+	gl_FragColor = mix(texture2D(color_texture, uv), fog_color, fog_factor);
+
+	//gl_FragColor = texture2D(color_texture, uv);
+	//gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- source/shaders/terrain-vertex.glsl
+++ source/shaders/terrain-vertex.glsl
@@ -8,9 +8,14 @@ uniform mat4 camera_projection;
 uniform vec3 camera_components[2]; // 0 = Target, 1 = Origin
 
 varying lowp vec2 uv;
+varying lowp vec3 vertex;
+varying lowp vec3 camera;
 
 void main()
 {
 	uv = vertex_uv;
+	vertex = vertex_position;
+	camera = camera_components[1];
+
 	gl_Position = (projection * camera_projection) * vec4(vertex_position, 1.0);
 }
